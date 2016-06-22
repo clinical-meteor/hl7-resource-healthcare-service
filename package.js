46d9c47a2446a1ee1828ca9d7d0cbc17a5112303
Abigail Watson <awatson1978@gmail.com>
2016-06-21 20:01:13 -0500

    bump

--- package.js
+++ package.js
@@ -17,12 +17,15 @@ Package.onUse(function (api) {
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.4.0');
+  api.use('clinical:hl7-resource-datatypes@0.4.4');
+  api.use('clinical:hl7-resource-organization@1.1.0');
+  api.use('clinical:hl7-resource-location@1.1.0');
 
   api.addFiles('lib/hl7-resource-healthcare-service.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
 
-  api.export('Observations');
-  api.export('ObservationSchema');
+  api.export('HealthcareService');
+  api.export('HealthcareServices');
+  api.export('HealthcareServiceSchema');
 });
