1378f6c5ce2e8454578ca6c79f1214eda3a0e714
Abigail Watson <awatson1978@gmail.com>
2017-04-01 00:31:09 -0500

    mocha tests

---
+++ tests/HealthcareServices.tests.js
@@ -0,0 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+import { assert } from 'meteor/practicalmeteor:chai';
+import { chai } from 'meteor/practicalmeteor:chai';
+import { expect } from 'meteor/practicalmeteor:chai';
+
+describe('clinical:hl7-family-healthcare-service', function () {
+  beforeEach(function () {
+    //console.log('beforeEach');
+  });
+  afterEach(function () {
+    //console.log('afterEach');
+  });
+  it('exists globally', function () {
+    expect(HealthcareServices).to.exist;
+  });
+});
+
